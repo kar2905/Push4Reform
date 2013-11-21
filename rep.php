8c5f76ac65cfe6520744fce93bfd28040567ff72
Kent Tam <ktam39@gmail.com>
2013-11-21 10:11:44 -0800

    fixed home page dimensions

--- rep.php
+++ rep.php
@@ -5,6 +5,7 @@
   <div class="profile-container container">
     <div class="row"> 
       <div class="col-md-3">
+        <img src="https://s3.amazonaws.com/push4reform/A000055.jpg">
       </div>
     </div>
   </div>

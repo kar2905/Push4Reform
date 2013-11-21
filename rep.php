687639d15842243b0d2f5e704b264e68be34ac1a
Kent Tam <ktam39@gmail.com>
2013-11-21 11:04:44 -0800

    updated styling on profile page

--- rep.php
+++ rep.php
@@ -4,8 +4,18 @@
 <div class="profile-page">
   <div class="profile-container container">
     <div class="row"> 
-      <div class="col-md-3">
-        <img src="https://s3.amazonaws.com/push4reform/A000055.jpg">
+      <div class="col-md-4">
+        <div class="clip">
+          <img src="https://s3.amazonaws.com/push4reform/A000055.jpg">
+        </div>
+        <button type="button" class="phone btn btn-lg btn-block"><span class="glyphicon glyphicon-earphone"></span>210-999-9999</button>
+        <button type="button" class="twitter btn btn-lg btn-block">@anneshoo</button>
+        <button type="button" class="fb btn btn-lg btn-block">Facebook</button>
+      </div>
+      <div class="col-md-8">
+        <h1>Anna Eshoo</h1>
+        <h2>Republican Representative</h2>
+        <h3>California</h3>
       </div>
     </div>
   </div>

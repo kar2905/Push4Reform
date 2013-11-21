1f1d6c1636c911130a7e1ff6ef82f1476bff0765
kar2905 <kar2905@gmail.com>
2013-11-20 23:49:05 -0800

    column fix

--- api/reps.php
+++ api/reps.php
@@ -2,7 +2,7 @@
 
 require("../dbinfo.php");
 
-$sql = "SELECT firstname,lastname, title, party, state, govtrack_id FROM sunlight";
+$sql = "SELECT firstname,lastname, title, party, state, bioguide_id FROM sunlight";
 $result = mysql_query($sql); 
 while($row = mysql_fetch_assoc($result)){
 	$rows[] = $row;

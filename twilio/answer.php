54e04d102cd800acd3bf878b4d6342db9dd31d3b
jayzalowitz <jayzalowitz@gmail.com>
2013-11-21 14:17:13 -0800

    twilio

---
+++ twilio/answer.php
@@ -0,0 +1,18 @@
+<?php
+ 
+    // make an associative array of callers we know, indexed by phone number
+
+    // if the caller is known, then greet them by name
+    // otherwise, consider them just another monkey
+         
+    // now greet the caller
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Gather numDigits="5" action="congress.php" method="POST">
+        <Say>To speak to your congessman about immigration reform, please enter your zip code.</Say>
+    </Gather>
+</Response>
+
+?>

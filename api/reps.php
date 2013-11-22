cc7b4a1bef218c1d0e2d0fc02f3ad7134a87f63d
kar2905 <kar2905@gmail.com>
2013-11-21 16:24:31 -0800

    new ratings

--- api/reps.php
+++ api/reps.php
@@ -9,14 +9,14 @@ if(isset($_GET['zip']))
 if(isset($_GET['state']))
 	$state=mysql_real_escape_string($_GET['state']);
 if(isset($zip)){
-$sql = "SELECT firstname,lastname, title, party, b.name_long as state, bioguide_id, rating FROM sunlight a, state b, congressratings c, congresszip d WHERE a.district = c.district and a.state = c.state and a.state = b.name_short  and a.district = d.district and a.state = d.state and d.zip = '".$zip."' LIMIT 20";
+$sql = "SELECT firstname,lastname, title, party, b.name_long as state, bioguide_id, rating FROM sunlight a, state b, congressratings_new c, congresszip d WHERE a.district = c.district and a.state = c.state and a.state = b.name_short  and a.district = d.district and a.state = d.state and d.zip = '".$zip."' LIMIT 20";
 }
 else if(isset($state)){
-$sql = "SELECT firstname,lastname, title, party, b.name_long as state, bioguide_id, rating FROM sunlight a, state b, congressratings c WHERE a.district = c.district and a.state = c.state and a.state = b.name_short  and b.name_long = '".$state."' LIMIT 20";
+$sql = "SELECT firstname,lastname, title, party, b.name_long as state, bioguide_id, rating FROM sunlight a, state b, congressratings_new c WHERE a.district = c.district and a.state = c.state and a.state = b.name_short  and b.name_long = '".$state."' LIMIT 20";
 
 }
 else{
-	$sql = "SELECT firstname,lastname, title, party, b.name_long as state, bioguide_id, rating FROM sunlight a, state b, congressratings c WHERE a.district = c.district and a.state = c.state and a.state = b.name_short  LIMIT 20";
+	$sql = "SELECT firstname,lastname, title, party, b.name_long as state, bioguide_id, rating FROM sunlight a, state b, congressratings_new c WHERE a.district = c.district and a.state = c.state and a.state = b.name_short  LIMIT 20";
 }
 $result = mysql_query($sql); 
 while($row = mysql_fetch_assoc($result)){

53620a8aa160a21c0209e0e8fe0ef4aa7c19ce3c
jmora258 <jmora258@gmail.com>
2014-01-23 01:05:13 -0800

    Update index.php
    oops

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-<?php
-$ip = $_SERVER['REMOTE_ADDR'];
-if(filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_IPV4)){
-  $json = file_get_contents('http://ipinfo.io/'.$ip.'/region');
-  $obj = json_decode($json);
-  $state = $obj;
-}
-else{
-  $state = "California";
-}
-
-  include 'includes/header.php';
-?>
 
 <div class="zip-container">
-  <h1>Where does your representative stand on immigration reform?</h1>
-  <h3>Click <a href="http://push.fwd.us">here</a> to see our updated version or visit http://push.fwd.us </h3> 
+ 
+	<div class="zip-container">
+	  <h1>Where does your representative stand on immigration reform?</h1>
+	  <h3>Click <a href="http://push.fwd.us">here</a> to see our updated version or visit http://push.fwd.us </h3> 
+	</div>
+
+<div>
+<div class="container reps-list" >
+  <div class="row" id="reps-list">
+  </div>
 </div>
 
 
